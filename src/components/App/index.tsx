32e719db9bb516521892f704b5eda2b5ad11676c
tmartin42 <japremieres2@gmail.com>
2019-04-09 14:58:00 +0200

    Initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const App = () => (
+	<div>
+		<h1>App</h1>
+	</div>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./index.css";
+import * as serviceWorker from "./registerServiceWorker";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
